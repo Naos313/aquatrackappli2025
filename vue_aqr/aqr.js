6da31aff8dd2f60d5a2f42c2677b07599e2a436f
Daux Julien <Julien.daux25@gmail>
2025-02-27 16:51:26 +0100

    vue aquarium ajouter et modifier fonctionnent

--- vue_aqr/aqr.js
+++ vue_aqr/aqr.js
@@ -19,6 +19,7 @@ webix.ui({
 			minWidth:200, 
 			select:true, 
 			url:"http://192.168.61.87:3000/aqr/", 
+			save :"json-> http://192.168.61.87:3000/aqr/",
 			datatype:"json",
 			template:"#id#) #nom#	(#acces#), #volume#L #photo# #date#",
 			on:{
@@ -46,7 +47,17 @@ webix.ui({
 				elements:[ 
 				  { view:"text", name:"nom", id:"inp_nom", label:"Nom de l'aquarium :" }, 
 				  { view:"text", name:"acces", id:"inp_acces", label:"Niveau d'accès :" },
-				  { view:"text", name:"date", id:"inp_date", label:"Date de création :" },
+				  /*{view:"combo", 
+					  name:"acces", 
+					  id:"inp_acces", 
+					  label:"Niveau d'accès :",
+					  value:"1", 
+					  options:[
+						{id:1, value:"privé"},
+						{id:2, value:"publique"}
+					] 
+					},*/
+				  { view:"datepicker", name:"date", id:"inp_date", label:"Date de création :" },
 				  { view:"text", name:"volume", id:"inp_volume", label:"Volume :" },
 				  { view:"text", name:"photo", id:"inp_photo", label:"Photo :" }, 
 				  { view:"text", name:"utl", id:"inp_id_utl", label:"Identifient de l'utilisateur :" },
@@ -61,13 +72,10 @@ webix.ui({
 			  elements:[
 				// autowidth is a specific feature of button and label
 				{ 
-					view:"button", id:"btn_cree", autowidth:true,  value:"Créer", click:ajouter
+					view:"button", id:"btn_modif", autowidth:true, value:"sauvegarder", click:modifier
 				},
 				{ 
 					view:"button", id:"btn_supr", autowidth:true, value:"Supprimer",
-				},
-				{ 
-					view:"button", id:"btn_modif", autowidth:true, value:"modifier", click:modifier
 				},
 				{ 
 					view:"button", id:"btn_clear", autowidth:true, value:"Clear", click:clearForm
@@ -86,36 +94,31 @@ webix.ui({
   ] 
 }); 
 
+
+
 function clearForm(){
   $$("aqr_form").clear();
   $$("aqr_prv_list").unselectAll();
 };
-function ajouter(){
-	webix.message("Création en cours");
-	var form = $$("aqr_form");
-	var form_data = form.getValues();
-	
-	webix.message(form_data.nom);
-};
 function modifier(){
 	var data= $$("aqr_prv_list");
 	var sel = data.getSelectedId(); 
-    if (!sel) return;
-    
 	var form = $$("aqr_form");
 	var form_data = form.getValues();
+	if (!form_data.nom) return;
+    if (!sel){
+		webix.message("existe pas");
+		data.add(form_data);
+    } else{
 	var item = data.getItem(sel);
     
     item = form_data;             // setting the new value for the item 
     data.updateItem(sel, item);     // the dataset is updated! 
-    webix.message("Bien"); 
-   
-    data.put("http://192.168.61.87:3000/aqr", { id : "2" }).then(function(data){
-    // response
-    console.log(data.text());
-});
+    webix.message("existe"); 
+	};
+};
 
-}
+
 function valuesToForm(id){
   var values = $$("aqr_prv_list").getItem(id);
   $$("aqr_form").setValues(values)
