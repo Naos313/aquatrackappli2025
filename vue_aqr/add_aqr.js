0497e0d87c25e3afa822cb1ca3df69cde917f5d2
Daux Julien <Julien.daux25@gmail>
2025-03-17 17:48:09 +0100

    vue aquarium (comentaire plus commande post - user_id + labelWidth)

--- vue_aqr/add_aqr.js
+++ vue_aqr/add_aqr.js
@@ -1,34 +1,37 @@
+// met la vue window dans window_ajouter
 var window_ajouter = webix.ui({
 					view:"window",
-					head:"Nouvel Aquarium",
-					width: 1000,
-					position:"center",
-					close:true,     
+					head:"Nouvel Aquarium",// nom affiché de la windows
+					width: 1000, // met la largeur de la window à 1000
+					position:"center", //affiche la window au centre de la page
+					close:true,  //affiche une croix pour fermer
+					//contenue de la window   
 					body:{ cols:[
-			{
+			{ //partie formulaire
 				view:"form", 
 				id:"aqr_form", 
 				gravity:1,
 				// elements == rows, cols can be declared instead 
-				elements:[ 
-				  { view:"text", name:"nom", id:"inp_nom", label:"Nom de l'aquarium :" }, 
+				elements:[
+				  { view:"text", name:"nom", id:"inp_nom", label:"Nom de l'aquarium :", labelWidth:160 }, // labelwidth est la largeur du libélé
 				  {view:"combo", 
 					  name:"acces", 
 					  id:"inp_acces", 
 					  label:"Niveau d'accès :",
-					  value:"privé", 
+					  value:"privé", // valeur par défaut
 					  options:[
 						{id:"privé", value:"privé"},
 						{id:"publique", value:"publique"}
-					] 
+					],
+					labelWidth:160 // labelwidth est la largeur du libélé
 					},
-				  { view:"datepicker",format:"%d/%m/%Y", name:"date", id:"inp_date", value: new Date(), label:"Date de création :" },
-				  { view:"text", name:"volume", id:"inp_volume", label:"Volume :" },
-				  { view:"text", name:"photo", id:"inp_photo", label:"Photo :" }, 
-				  { view:"text", name:"utl", id:"inp_id_utl", label:"Identifient de l'utilisateur :" },
+				  { view:"datepicker",format:"%d/%m/%Y", name:"date", id:"inp_date", value: new Date(), label:"Date de création :", labelWidth:160 }, //value affiche a la date du jour 
+				  { view:"text", name:"volume", id:"inp_volume", label:"Volume :", labelWidth:160 },
+				  { view:"text", name:"photo", id:"inp_photo", label:"Photo :", labelWidth:160 }, 
 				  {} 
 				] 
 			},
+			// bouton sauvegarder
 			{view:"toolbar",
 			  id:"bouton",
 			  
@@ -36,9 +39,9 @@ var window_ajouter = webix.ui({
 			  elements:[
 				// autowidth is a specific feature of button and label
 				{ margin:5, rows:[
-					{},
+					{},//affiche le bouton en bas
 					{ 
-						view:"button", id:"btn_modif", autowidth:true, height:80, value:"sauvegarder", click:modifier
+						view:"button", id:"btn_modif", autowidth:true, height:80, value:"sauvegarder", click:modifier // appel la fonction modifier
 					},
 				]}
 				  
--- vue_aqr/aqr.js
+++ vue_aqr/aqr.js
@@ -1,78 +1,84 @@
-var format_date = webix.Date.dateToStr("%d/%m/%Y");
-var n_private =7;
-
+var format_date = webix.Date.dateToStr("%d/%m/%Y");// formmat de la date
+var API_URL = "http://192.168.61.87:3000/aqr"; //url api
 webix.ui({ 
 
   rows:[
+  //bouton Nouveau
 	{cols:[
 		{},
-		{view:"button", id:"btn_nouv", autowidth:true, value:"Nouveau",click:affiche_add_window}
+		{view:"button", id:"btn_nouv", autowidth:true, value:"Nouveau",click:affiche_add_window} //ouvre une nouvelle windows (add_aqr.js)
 	]},
     { 
 		cols:[
+		// liste mes aquariums
 			{view:"datatable", 
 			id:"aqr_prv_list", 
 			minWidth:200, 
 
 			select:true, 
+			//charge les données
 			url:function(params){
 				return webix.ajax().get("http://192.168.61.87:3000/aqr/", params).then(function(data){
 					data = data.json();
+					//met la taille des ligne à 60
 					for ( let ligne of data){
 						ligne.$height = 60;
 					}
 					return data;});
 			}, 
+			//enregitre les modification (à modifier)
 			save :"json-> http://192.168.61.87:3000/aqr/",
 			datatype:"json",
 			//template:"#id#) #nom#	(#acces#), #volume#L #photo# #date#",
 			columns:[
 			{
 				id: "nom", fillspace: true, 
-				header:[{text:"Mes aquarium", colspan:5}, "Nom" ],
-				css:"titleColumn",
+				header:[{text:"Mes aquariums", colspan:5}, "Nom" ] // nom de la table + de la colonne Nom
 			},
 			{
 				id: "acces", fillspace: true,
-				header:[ {text:""}, {text:"Accés"} ] 
+				header:[ {text:""}, {text:"Accés"} ] //espace pour le nom de la table + nom de la colonne Accés
 			},
 			{
 				id: "volume",fillspace: true, 
-				header:[ {text:""}, {text:"Volumes"} ],
+				header:[ {text:""}, {text:"Volumes"} ],//espace pour le nom de la table + nom de la colonne Volumes
 				template: function (obj) {
-					return "<span>" + obj.volume + " L</span>"
+					return "<span>" + obj.volume + " L</span>" //renvoi les données sous la forme "volume L"
 				}
 			},
 			{
 				id: "photo", 
 					fillspace: true, 
-					header:[ {text:""}, {text:"Photo"} ],
+					header:[ {text:""}, {text:"Photo"} ],//espace pour le nom de la table + nom de la colonne Photo
 					width:150,
 					template: function (obj) {
-						return '<img src="'+obj.photo+'" height=100%/>'
+						return '<img src="'+obj.photo+'" height=100%/>' // affiche l'image avec comme hauteur celle de la colonne
 					  }
 			},
 			{
 				id: "date", fillspace: true,
-				header:[ {text:""}, {text:"Date"} ],
-				format:format_date
+				header:[ {text:""}, {text:"Date de création"} ],//espace pour le nom de la table + nom de la colonne Date de création
+				format:format_date //affiche au format jj/mm/aaaa
 			}
 			],
-			/*on:{
+			//Quand on selectionne une ligne
+			/*on:{ 
 				onAfterSelect:valuesToForm
 			}*/
 			},
+			// liste aquariums publics
 			{view:"datatable", 
 			id:"aqr_plq_list", 
 			minWidth:200,
 
 			select:true, 
 			autoConfig:true,
+			//charge les données
 			url:function(params){
 				return webix.ajax().get("http://192.168.61.87:3000/aqr_pub/", params).then(function(data){
 					data = data.json();
 					for ( let ligne of data){
-						ligne.$height = 60;
+						ligne.$height = 60; //met la taille des ligne à 60
 					}
 					return data;});
 			}, 
@@ -80,32 +86,28 @@ webix.ui({
 			columns:[
 			{
 				id: "nom", fillspace: true, 
-				header:[{text:"Aquarium publiques", colspan:5}, "Nom" ]
-			},
-			{
-				id: "acces", fillspace: true,
-				header:[ {text:""}, {text:"Accés"} ] 
+				header:[{text:"Aquariums publics", colspan:4}, "Nom" ] // nom de la table + de la colonne Nom
 			},
 			{
 				id: "volume",fillspace: true, 
-				header:[ {text:""}, {text:"Volumes"} ],
+				header:[ {text:""}, {text:"Volumes"} ], //espace pour le nom de la table + nom de la colonne Volumes
 				template: function (obj) {
-					return "<span>" + obj.volume + " L</span>"
+					return "<span>" + obj.volume + " L</span>" //renvoi les données sous la forme "volume L"
 				}
 			},
 			{
 				id: "photo", 
 				fillspace: true, 
-				header:[ {text:""}, {text:"Photo"} ],
+				header:[ {text:""}, {text:"Photo"} ], //espace pour le nom de la table + nom de la colonne Photo
 				width:150,
 				template: function (obj) {
-					return '<img src="'+obj.photo+'" height=100%/>'
+					return '<img src="'+obj.photo+'" height=100%/>' // affiche l'image avec comme hauteur celle de la colonne
 				  }
 			},
 			{
 				id: "date", fillspace: true,
-				header:[ {text:""}, {text:"Date"} ],
-				format:format_date  
+				header:[ {text:""}, {text:"Date de création"} ], //espace pour le nom de la table + nom de la colonne Date de création
+				format:format_date  //affiche au format jj/mm/aaaa
 			}
 			],
 			}
@@ -168,28 +170,46 @@ webix.ui({
   ] 
 }); 
 
+//ouvre une nouvelle windows (add_aqr.js)
 function affiche_add_window(){
 				window_ajouter.show();
+				var form = $$("aqr_form"); //selectionne le formulaire de la windows dans form
+				form.clear(); //vide le formulaire
 			};
 
 /*function clearForm(){
   $$("aqr_form").clear();
   $$("aqr_prv_list").unselectAll();
 };*/
+
+//rajoute un aquarium
 function modifier(){
-	var data= $$("aqr_prv_list");
+	var data= $$("aqr_prv_list"); //selectionne la liste  mes aquariums dans data
 	
-	var form = $$("aqr_form");
-	var form_data = form.getValues();
+	var form = $$("aqr_form"); //selectionne le formulaire de la windows dans form
+	var form_data = form.getValues(); // met les données du formulaire dans form_data
 	if (!form_data.nom || !form_data.acces || !form_data.date || !form_data.volume){
-		webix.message("Le formulaire n'est pas rempli");
+		webix.message("Le formulaire n'est pas rempli");// affiche un message d'erreur si les champs ne son pas remplient
 		return;
 	}
+	let newObservation = { 
+		nom: form_data.nom, // Récupération du nom 
+		date: webix.Date.dateToStr("%Y-%m-%d")(form_data.date), // Formatage de la date 
+		acces: form_data.acces,// Récupération de l'accés 
+		volume: form_data.volume,// Récupération du volume
+		photo: form_data.photo,// Récupération de la photo
+		user_id: "1" // met l'user_id à 1 (à modifier)
+	}; 
+		// Envoi des données via AJAX en méthode POST avec conversion en JSON 
+	webix.ajax().post(API_URL, JSON.stringify(newObservation), { 
+		headers: { "Content-Type": "application/json" } // Envoi en JSON 
+	});
+	
 	//webix.message(item.nom + " a été ajouté");
-	data.add(form_data);
-	webix.message("L'aquarium " + form_data.nom + " a été ajouté"); 
-	window_ajouter.hide();
-	form.clear();
+	//data.add(form_data); //rajoute un aquarium avec les données du formulaire
+	webix.message("L'aquarium " + form_data.nom + " a été ajouté");  // affiche un message de comfirmation
+	//~ $$("aqr_prv_list").load(API_URL);  // rechache la liste
+	window_ajouter.hide(); //masque la window
 };
 /*function supprimer(){
 	var list = $$("aqr_prv_list");
