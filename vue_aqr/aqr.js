36b4eaaf87d78039c457500a5b28bd4f53d02650
Daux Julien <Julien.daux25@gmail>
2025-04-01 08:38:14 +0200

    vue aquarium (retraits de commentaire

--- vue_aqr/aqr.js
+++ vue_aqr/aqr.js
@@ -113,59 +113,6 @@ webix.ui({
 			}
 		]
 }, 
-/*{  
-	cols:[
-			{
-				view:"form", 
-				id:"aqr_form", 
-				gravity:1,
-				// elements == rows, cols can be declared instead 
-				elements:[ 
-				  { view:"text", name:"nom", id:"inp_nom", label:"Nom de l'aquarium :" }, 
-				  {view:"combo", 
-					  name:"acces", 
-					  id:"inp_acces", 
-					  label:"Niveau d'accès :",
-					  value:"privé", 
-					  options:[
-						{id:"privé", value:"privé"},
-						{id:"publique", value:"publique"}
-					] 
-					},
-				  { view:"datepicker",format:"%d/%m/%Y", name:"date", id:"inp_date", label:"Date de création :" },
-				  { view:"text", name:"volume", id:"inp_volume", label:"Volume :" },
-				  { view:"text", name:"photo", id:"inp_photo", label:"Photo :" }, 
-				  { view:"text", name:"utl", id:"inp_id_utl", label:"Identifient de l'utilisateur :" },
-				  {} 
-				] 
-			},
-			{view:"toolbar",
-			  id:"bouton",
-			  
-			  // elements == cols, rows can be declared instead
-			  elements:[
-				// autowidth is a specific feature of button and label
-				{ margin:5, rows:[
-					{ 
-						view:"button", id:"btn_modif", autowidth:true, height:80, value:"sauvegarder", click:modifier
-					},
-					{ 
-						view:"button", id:"btn_supr", height:80, value:"Supprimer", click:supprimer
-					},
-					{ 
-						view:"button", id:"btn_clear", height:80, value:"Effacer", click:clearForm
-						
-					},
-				]}
-				  
-			  ]
-				
-			}
-		]
-  }*/
-
-
-
 
   ] 
 }); 
@@ -176,11 +123,6 @@ function affiche_add_window(){
 				var form = $$("aqr_form"); //selectionne le formulaire de la windows dans form
 				form.clear(); //vide le formulaire
 			};
-
-/*function clearForm(){
-  $$("aqr_form").clear();
-  $$("aqr_prv_list").unselectAll();
-};*/
 
 //rajoute un aquarium
 function ouvre_vue_aqr(){
